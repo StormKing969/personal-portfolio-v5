ab1f14a2e7285a35d275a495b886c38a0c294c09
Sajana <svwijesinghe97@gmail.com>
2025-10-27 18:45:17 -0400

    Project Update I

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -54,7 +54,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.8 }}
           >
-            Web Solutions
+            AI Enabled Web Solutions
           </motion.p>
         </div>
       </div>
